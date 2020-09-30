297d1e8e4a0d7d14aeb56ae11e858bc86e48691a
arthur <arthur@pop-os.localdomain>
2020-09-30 13:24:43 -0300

    Criada a estrutura do projeto

---
+++ src/main/java/com/arthurdias/desafio01/desafio01/Desafio01Application.java
@@ -0,0 +1,13 @@
+package com.arthurdias.desafio01.desafio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+}
